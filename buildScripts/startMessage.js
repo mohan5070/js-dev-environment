c06ff62fce4417abebbf6c767756be0d90b8aa0c
Mohan <mohan@cloudthing.com>
2017-07-20 12:25:01 +0530

    babel transpiler added

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -2,5 +2,6 @@
  * Created by mohanr on 7/19/2017.
  */
 
-var chalk = require('chalk');
+import chalk from 'chalk';
+
 console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
